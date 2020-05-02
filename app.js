bc2b677126d37faf54ec70de31596005ae71808b
mofe64 <ogunbiyioladapo33@gmail.com>
2020-05-02 15:27:30 +0100

    categories routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const APPError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorContoller');
+const categoryRouter = require('./routers/categoryRoutes');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.json({ limit: '10kb' }));
 app.use(bodyparser.urlencoded({ limit: '10mb', extended: false }));
 
 //routes
-
+app.use('/api/v1/category', categoryRouter);
 //errors
 app.use(globalErrorHandler);
 
